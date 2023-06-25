426207851070d0a3060d18dbb48cbf1ef65b8a2d
Trevor Sutherland <trevor.sutherland@gmail.com>
2023-06-25 00:20:05 -0400

    Update to browser open page

--- src/PianoApp.go
+++ src/PianoApp.go
@@ -18,7 +18,7 @@ func main() {
 	}()
 
 	// Open the browser
-	openBrowser("http://localhost:8080/PianoVAE.html")
+	openBrowser("http://localhost:8080/src/PianoVAE.html")
 
 	// Block forever
 	select {}
