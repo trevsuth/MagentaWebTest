eb53fcb0653186d395ecd2b7f81df1e07ef77711
Trevor Sutherland <trevor.sutherland@gmail.com>
2023-06-24 23:46:41 -0400

    Updated PianoApp.go to open web browser  Please enter the commit message for your changes. Lines starting

--- PianoApp.go
+++ PianoApp.go
@@ -3,16 +3,42 @@ package main
 import (
 	"log"
 	"net/http"
+	"os/exec"
+	"runtime"
 )
 
 func main() {
-	fs := http.FileServer(http.Dir("./"))
-	http.Handle("/", fs)
+	go func() {
+		// Start the server
+		http.Handle("/", http.FileServer(http.Dir("./")))
+		err := http.ListenAndServe(":8080", nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
 
-	log.Println("Listening on :8080...")
-	err := http.ListenAndServe(":8080", nil)
+	// Open the browser
+	openBrowser("http://localhost:8080")
+
+	// Block forever
+	select {}
+}
+
+func openBrowser(url string) {
+	var cmd *exec.Cmd
+
+	switch runtime.GOOS {
+	case "windows":
+		cmd = exec.Command("cmd", "/c", "start", url)
+	case "darwin":
+		cmd = exec.Command("open", url)
+	default: // "linux", "freebsd", "openbsd", "netbsd"
+		cmd = exec.Command("xdg-open", url)
+	}
+
+	err := cmd.Start()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Failed to open browser: %v", err)
 	}
 }
 
